9d551a00fe2beb7fe2d17b62303b482d249a8474
IBAB Student <ibab@IBAB-MScBDB04-Comp04.ibab.ac.in>
2026-09-03 18:33:16 +0530

    Lab15 work

---
+++ Lab15/Lab15.py
@@ -0,0 +1,29 @@
+# lab15 assignments sept 03
+
+
+# question 3
+#k = int(input("enter positive your integer: "))
+#n = int(input("enter your integer: "))
+#if n > 0:
+#    a=k+n
+#    b=k-n
+#    c=k*n
+#    d=k/n
+#    print("addition:",a)
+#    print("subtraction:",b)
+#    print("multiplication:",c)
+#    print("division:",d)
+
+
+# question 4
+"""import math
+a = float(input('Enter the number for the place "a": '))
+b = float(input('Enter the number for the place "b": '))
+c = float(input('Enter the number for the place "c": '))
+
+x=b*b - 4*a*c
+y=math.sqrt(x)
+solution1=(-y)/(2*a)
+solution2=(+y)/(2*a)
+print(solution1)
+print(solution2)"""
